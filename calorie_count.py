97b65fa487e042e8a9413b853304b1ba4f1c2ded
Andres S <suprematis@localhost.localdomain>
2023-03-06 01:44:56 -0800

    Figured out a way to sum the ranges.  Just need to figure out an algorithm to take the index of the nulls and generate a sum of all the ranges after each NaN

--- calorie_count.py
+++ calorie_count.py
@@ -4,14 +4,23 @@ import pandas as pd
 df = pd.read_csv ('elf_count.csv', skip_blank_lines=False)
 empties = np.where(pd.isnull(df))
 np.where(df.applymap(lambda x: x == ''))
-print(empties)
+#print(empties)
 
 pd.set_option('expand_frame_repr', True)
 # calling head() method  
 # storing in new variable 
 data_top = df.head() 
-print(df.iloc[2221,0])
+print(df.iloc[14,0])
+nulls = df[df['calorie_count'].isnull()].index.tolist()
+#for i in nulls:
+#   print(df.iloc[i-1,0])
 
+
+sum = df.iloc[0:14].sum()
+print(sum)
+
+
+#print(nulls)
 # display 
 #print(data_top)
 
